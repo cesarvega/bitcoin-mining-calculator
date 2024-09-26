2e20f1a4a4f1a4df20426324ba6559521c197e66
ElevateTechIA <169070291+ElevateTechIA@users.noreply.github.com>
2024-09-26 14:34:08 -0400

    Update RequestCar.tsx

--- src/components/RequestCar.tsx
+++ src/components/RequestCar.tsx
@@ -7,7 +7,7 @@ export function RequestCar() {
         <div className=" w-full h-screen bg-gray-900 flex  ">
 
 <div className='flex flex-col  mt-10 gap-4 p-4'>
-    <img src="/public/logo.jpeg" alt="logo omniparking" />
+    <img src="/logo.jpeg" alt="logo omniparking" />
             <button onClick={()=> console.log(id)} className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded-full transition duration-300 ease-in-out transform hover:scale-105 focus:outline-none">
                 Request My Car
             </button>
