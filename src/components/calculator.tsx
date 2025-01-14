f3082a73de9c90acd515da33580a356f2f49835c
cesarvega <cesarvega.col@gmail.com>
2025-01-14 16:24:06 -0500

    fixed layout details

--- src/components/calculator.tsx
+++ src/components/calculator.tsx
@@ -1,4 +1,4 @@
-import { FormEvent, useState } from "react";
+import { FormEvent, useEffect, useState } from "react";
 import { MiningFormData, MiningResult } from "../types";
 import MiningResultCard from "./miningResults";
 
@@ -30,6 +30,7 @@ export function Calculator() {
     const [response, setResponse] = useState<any>(null);
     const [error, setError] = useState<string | null>(null);
 
+    const [isFormValid, setIsFormValid] = useState<boolean>(false);
     // Handles changes in the form fields
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         const { name, value } = e.target;
@@ -38,7 +39,16 @@ export function Calculator() {
             [name]: Number(value),
         });
     };
-
+    useEffect(() => {
+        // Check if all fields have valid values
+        const isValid = Object.values(formData).every(
+          
+            (value) => {typeof value === "string" ? value.trim() !== "0" : value !== "0"
+                console.log(value)
+            }
+        );
+        setIsFormValid(isValid);
+    }, [formData]);
     // Handles form submission
     const handleSubmit = async (e: FormEvent) => {
         e.preventDefault();
@@ -60,7 +70,6 @@ export function Calculator() {
             }
 
             const data = await res.json();
-            console.log(data);
             setResponse(data);
         } catch (err: any) {
             setError(err.message || 'An error occurred');
@@ -72,112 +81,106 @@ export function Calculator() {
     return (
         <div className="flex flex-col lg:flex-row items-center justify-center min-h-screen py-6 bg-gray-900">
             <div className="mx-10 w-full lg:w-auto">
-                <form
-                    className="bg-black p-4 rounded-lg  lg:px-8 shadow-md w-full lg:w-auto mx-auto space-y-4"
-                    onSubmit={handleSubmit}
+        <form
+            className="bg-black p-4 rounded-lg lg:px-8 shadow-md w-full lg:w-auto mx-auto space-y-4"
+            onSubmit={handleSubmit}
+        >
+            <div>
+                <label
+                    htmlFor="hash_rate"
+                    className="block text-white font-semibold mb-2"
                 >
-                    <div>
-                        <label
-                            htmlFor="hash_rate"
-                            className="block text-white font-semibold mb-2"
-                        >
-                            Hash Rate (H/s):
-                        </label>
-                        <input
-                            type="number"
-                            id="hash_rate"
-                            name="hash_rate"
-                            value={formData.hash_rate > 0 ? formData.hash_rate : ''}
-                            onChange={handleChange}
-                            required
-                            min="0"
-                            className="w-full px-3 py-2 border border-gray-400 rounded focus:outline-none focus:ring focus:ring-yellow-500"
-                        />
-                    </div>
+                    Hash Rate (H/s):
+                </label>
+                <input
+                    type="number"
+                    id="hash_rate"
+                    name="hash_rate"
+                    value={formData.hash_rate > 0 ? formData.hash_rate :"" }
+                    onChange={handleChange}
+                    required
+                    min="0"
+                    className="w-full px-3 py-2 border border-gray-400 rounded focus:outline-none focus:ring focus:ring-yellow-500"
+                />
+            </div>
 
-                    <div>
-                        <label
-                            htmlFor="power_consumption"
-                            className="block text-white font-semibold mb-2"
-                        >
-                            Power Consumption (W):
-                        </label>
-                        <input
-                            type="number"
-                            id="power_consumption"
-                            name="power_consumption"
-                            value={
-                                formData.power_consumption > 0 ? formData.power_consumption : ''
-                            }
-                            onChange={handleChange}
-                            required
-                            min="0"
-                            className="w-full px-3 py-2 border border-gray-400 rounded focus:outline-none focus:ring focus:ring-yellow-500"
-                        />
-                    </div>
+            <div>
+                <label
+                    htmlFor="power_consumption"
+                    className="block text-white font-semibold mb-2"
+                >
+                    Power Consumption (W):
+                </label>
+                <input
+                    type="number"
+                    id="power_consumption"
+                    name="power_consumption"
+                    value={formData.power_consumption > 0 ? formData.power_consumption : "" }
+                    onChange={handleChange}
+                    required
+                    min="0"
+                    className="w-full px-3 py-2 border border-gray-400 rounded focus:outline-none focus:ring focus:ring-yellow-500"
+                />
+            </div>
 
-                    <div>
-                        <label
-                            htmlFor="electricity_cost"
-                            className="block text-white font-semibold mb-2"
-                        >
-                            Electricity Cost (USD/kWh):
-                        </label>
-                        <input
-                            type="number"
-                            step="0.01"
-                            id="electricity_cost"
-                            name="electricity_cost"
-                            value={
-                                formData.electricity_cost > 0 ? formData.electricity_cost : ''
-                            }
-                            onChange={handleChange}
-                            required
-                            min="0"
-                            className="w-full px-3 py-2 border border-gray-400 rounded focus:outline-none focus:ring focus:ring-yellow-500"
-                        />
-                    </div>
+            <div>
+                <label
+                    htmlFor="electricity_cost"
+                    className="block text-white font-semibold mb-2"
+                >
+                    Electricity Cost (USD/kWh):
+                </label>
+                <input
+                    type="text"
+                    step="0.01"
+                    id="electricity_cost"
+                    name="electricity_cost"
+                    value={formData.electricity_cost > 0 ? formData.electricity_cost : ""}
+                    onChange={handleChange}
+                    required
+                    min="0"
+                    className="w-full px-3 py-2 border border-gray-400 rounded focus:outline-none focus:ring focus:ring-yellow-500"
+                />
+            </div>
 
-                    <div>
-                        <label
-                            htmlFor="initial_investment"
-                            className="block text-white font-semibold mb-2"
-                        >
-                            Initial Investment (USD):
-                        </label>
-                        <input
-                            type="number"
-                            id="initial_investment"
-                            name="initial_investment"
-                            value={
-                                formData.initial_investment > 0
-                                    ? formData.initial_investment
-                                    : ''
-                            }
-                            onChange={handleChange}
-                            required
-                            min="0"
-                            className="w-full px-3 py-2 border border-gray-400 rounded focus:outline-none focus:ring focus:ring-yellow-500"
-                        />
-                    </div>
+            <div>
+                <label
+                    htmlFor="initial_investment"
+                    className="block text-white font-semibold mb-2"
+                >
+                    Initial Investment (USD):
+                </label>
+                <input
+                    type="number"
+                    id="initial_investment"
+                    name="initial_investment"
+                    value={formData.initial_investment > 0 ? formData.initial_investment : ""}
+                    onChange={handleChange}
+                    required
+                    min="0"
+                    className="w-full px-3 py-2 border border-gray-400 rounded focus:outline-none focus:ring focus:ring-yellow-500"
+                />
+            </div>
 
-                    <button
-                        type="submit"
-                        disabled={isSubmitting}
-                        className={`w-full py-2 px-4 rounded text-white font-semibold ${isSubmitting
-                            ? 'bg-gray-400 cursor-not-allowed'
-                            : 'bg-yellow-600 hover:bg-yellow-500'
-                            }`}
-                    >
-                        {isSubmitting ? 'Sending...' : 'Calculate'}
-                    </button>
-
-                    {error && (
-                        <div className="text-red-600 mt-4">
-                            <p>{error}</p>
-                        </div>
-                    )}
-                </form>
+            <button
+                type="submit"
+                disabled={ isSubmitting}
+                className={`w-full py-2 px-4 rounded text-white font-semibold ${
+                    isSubmitting
+                        ? "bg-gray-400 "
+                        : "bg-yellow-600 "
+                }`}
+            >
+                {isSubmitting ? "Sending..." : "Calculate"}
+                
+            </button>
+          
+            {error && (
+                <div className="text-red-600 mt-4">
+                    <p>{error}</p>
+                </div>
+            )}
+        </form>
             </div>
             <div className="mx-10 w-full lg:w-auto">
                 {response ? <MiningResultCard data={response} /> : <MiningResultCard data={initialData} />}</div>
